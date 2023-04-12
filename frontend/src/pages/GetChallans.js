cbf07431e036e4a4c90f3d27c53a9a7fbc2958a3
Keyur3766 <keyurnagar9453@gmail.com>
2023-04-12 22:33:56 +0530

    Challan details page fully completed

--- frontend/src/pages/GetChallans.js
+++ frontend/src/pages/GetChallans.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import Table from '@mui/material/Table';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
@@ -10,8 +10,9 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useLocation } from 'react-router-dom';
 import UserServices from '../services/UserServices';
+// import { OverviewBudget } from 'src/sections/@dashboard/user/overview-budget';
 
-const TAX_RATE = 0.07;
+const TAX_RATE = 0.18;
 
 function ccyFormat(num) {
   return `${num.toFixed(2)}`;
@@ -41,113 +42,148 @@ const invoiceTaxes = TAX_RATE * invoiceSubtotal;
 const invoiceTotal = invoiceTaxes + invoiceSubtotal;
 
 export default function GetChallans(props) {
-
   const location = useLocation();
-  const { id } = location.state || {}; 
-  
+  const { id } = location.state || {};
+
   const [loading, setLoading] = useState(true);
 
   const [data, setData] = useState();
   const groupedData = {};
-  const [filteredData,setfilteredData] = useState(null);
-  const GetChallanData = (my_id) => {  
-    try{
+  const subtotalData = {};
+  const [filteredData, setfilteredData] = useState(null);
+  const [filteredsubtotal, setfilteredsubtotal] = useState(null);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+  
+    // Extract the day from the formatted date
+    const day = Number(formattedDate.split(' ')[0]);
+  
+    // Add "th", "st", "nd", or "rd" suffix based on the day
+    let daySuffix;
+    if (day === 1 || day === 21 || day === 31) {
+      daySuffix = 'st';
+    } else if (day === 2 || day === 22) {
+      daySuffix = 'nd';
+    } else if (day === 3 || day === 23) {
+      daySuffix = 'rd';
+    } else {
+      daySuffix = 'th';
+    }
+  
+    // Replace the day in the formatted date with the formatted day
+    const formattedDay = `${day}${daySuffix}`;
+    const formattedDateWithDay = formattedDate.replace(day, formattedDay);
+  
+    return formattedDateWithDay;
+  };
+  
+
+  const GetChallanData = (my_id) => {
+    try {
       UserServices.Get_ChallanDetailsById(my_id).then((res) => {
         const resdata = res.data;
         console.warn(resdata);
         setData(resdata);
-        resdata.forEach((item)=>{
-          if(!groupedData[item.issue_date]){
+        resdata.forEach((item) => {
+          if (!groupedData[item.issue_date]) {
             groupedData[item.issue_date] = [];
+            subtotalData[item.issue_date] = 0;
           }
           groupedData[item.issue_date].push(item);
-
-          
-        })
-
+          subtotalData[item.issue_date] += (item.totalQuantity * item.item.selling_price);
+        });
+        console.warn(subtotalData);
         setfilteredData(groupedData);
-        console.log(groupedData[resdata[0].issue_date]); 
-        
+        setfilteredsubtotal(subtotalData);
       });
-      
-      
-    }
-    catch(error){
+    } catch (error) {
       console.log(error);
     }
   };
-  
- 
 
-useEffect(() => {
-  GetChallanData(id);
-  // setLoading(false);
+  useEffect(() => {
+    GetChallanData(id);
+    // setLoading(false);
+  }, []);
 
-}, []);
-
-
-  if(filteredData==null){
-    console.log("runnig");
-    return (<div>loading....</div>);
+  if (filteredData == null) {
+    console.log('runnig');
+    return <div>loading....</div>;
   }
-  console.warn(typeof(filteredData));
+  if(Object.keys(filteredData).length===0){
+    return <div>No data found</div>
+  }
+  
+  console.warn(filteredData);
+  console.log(filteredsubtotal);
+
+  const subtotal = 0;
   return (
     <>
-    
-    <Typography variant="h4" component="h2" sx={{ml:6}}>
-        Date: 9th April, 2023
-    </Typography>
-    <br/>
-    <TableContainer sx={{width:900, mx:'auto'}} component={Paper}>
-      <Table  sx={{ minWidth: 700}} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" colSpan={3}>
-              Details
-            </TableCell>
-            <TableCell align="right">Price</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Desc</TableCell>
-            <TableCell align="right">Quantity.</TableCell>
-            <TableCell align="right">Unit</TableCell>
-            <TableCell align="right">Sum</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-      
-          {filteredData[data[0].issue_date].map((row) => (
-            <TableRow key={row.challan_id}>
-              <TableCell>{row.item.Name}</TableCell>
-              <TableCell align="right">{row.totalQuantity}</TableCell>
-              <TableCell align="right">{row.item.selling_price}</TableCell>
-              <TableCell align="right">{ccyFormat(10)}</TableCell>
-            </TableRow>
-          ))}
+      {/* <OverviewBudget
+              difference={12}
+              positive
+              sx={{ height: '100%' }}
+              value="$24k"
+            /> */}
+      {Object.entries(filteredData).map(([key, value]) => (
+        <>
+          <Typography key={key} variant="h4" component="h2" sx={{ ml: 6 }}>
+            Date: {formatDate(key)}
+          </Typography>
 
-          <TableRow>
-            <TableCell rowSpan={3} />
-            <TableCell colSpan={2}>Subtotal</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>Tax</TableCell>
-            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell colSpan={2}>Total</TableCell>
-            <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+          <br />
+          <TableContainer sx={{ width: 900, mx: 'auto' }} component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center" colSpan={3}>
+                    Details
+                  </TableCell>
+                  <TableCell align="right">Price</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Desc</TableCell>
+                  <TableCell align="right">Quantity.</TableCell>
+                  <TableCell align="right">Unit</TableCell>
+                  <TableCell align="right">Sum</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {value.map((row) => (
+                  <TableRow key={row.challan_id}>
+                    <TableCell>{row.item.Name}</TableCell>
+                    <TableCell align="right">{row.totalQuantity}</TableCell>
+                    <TableCell align="right">{row.item.selling_price}</TableCell>
+                    <TableCell align="right">{ccyFormat(row.totalQuantity*row.item.selling_price)}</TableCell>
+                  </TableRow>
+                ))}
 
-            <br/>
-            <br/>
+                <TableRow>
+                  <TableCell rowSpan={3} />
+                  <TableCell colSpan={2}>Subtotal</TableCell>
+                  <TableCell align="right">{ccyFormat(Number(filteredsubtotal[key]))}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Tax</TableCell>
+                  <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
+                  <TableCell align="right">{ccyFormat(Number(filteredsubtotal[key]*0.18))}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={2}>Total</TableCell>
+                  <TableCell align="right">{ccyFormat(Number(filteredsubtotal[key]*1.18))}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
 
-    
-
-    </>
+          <br />
+          <br />
+          </>
+      ))}
+      </>
   );
 }
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -45,14 +45,14 @@ export default function Router() {
       element: <SimpleLayout />,
       children: [
         { element: <Navigate to="/dashboard/app" />, index: true },
-        // { path: '404', element: <Page404 /> },
-        // { path: '*', element: <Navigate to="/404" /> },
+        { path: '404', element: <Page404 /> },
+        { path: '*', element: <Navigate to="/404" /> },
       ],
     },
-    // {
-    //   path: '*',
-    //   element: <Navigate to="/404" replace />,
-    // },
+    {
+      path: '*',
+      element: <Navigate to="/404" replace />,
+    },
   ]);
 
   return routes;
